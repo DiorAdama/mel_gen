36f7e5f2797da2299eef2634202c3e2fdf6603d2
fadel <fadelsougou@gmail.com>
2022-06-26 18:36:58 +0200

    feat mel feature representation

---
+++ src/mel_RNN.py
@@ -0,0 +1,89 @@
+import tensorflow as tf 
+
+# Download and import the MIT 6.S191 package
+import mitdeeplearning as mdl
+
+# Import all remaining packages
+import numpy as np
+import os
+import time
+import functools
+from IPython import display as ipythondisplay
+from tqdm import tqdm
+
+#print(len(tf.config.list_physical_devices('GPU')))
+
+
+# Download the dataset
+songs = mdl.lab1.load_training_data()
+
+# Print one of the songs to inspect it in greater detail!
+example_song = songs[0]
+print("\nExample song: ")
+print(example_song)
+
+
+# Join our list of song strings into a single string containing all songs
+songs_joined = "\n\n".join(songs) 
+
+# Find all unique characters in the joined string
+vocab = sorted(set(songs_joined))
+print("There are", len(vocab), "unique characters in the dataset")
+
+
+char2idx = {u:i for i, u in enumerate(vocab)}
+idx2char = np.array(vocab)
+
+print('{')
+for char,_ in zip(char2idx, range(20)):
+    print('  {:4s}: {:3d},'.format(repr(char), char2idx[char]))
+print('  ...\n}')
+
+
+def vectorize_string(string):
+    return np.array([char2idx[c] for c in string])
+
+
+vectorized_songs = vectorize_string(songs_joined)
+
+print ('{} ---- characters mapped to int ----> {}'.format(repr(songs_joined[:10]), vectorized_songs[:10]))
+# check that vectorized_songs is a numpy array
+assert isinstance(vectorized_songs, np.ndarray), "returned result should be a numpy array"
+
+
+
+### Batch definition to create training examples ###
+
+def get_batch(vectorized_songs, seq_length, batch_size):
+  # the length of the vectorized songs string
+  n = vectorized_songs.shape[0] - 1
+  # randomly choose the starting indices for the examples in the training batch
+  idx = np.random.choice(n-seq_length, batch_size)
+
+  '''TODO: construct a list of input sequences for the training batch'''
+  input_batch = [vectorized_songs[i:i+seq_length] for i in idx]
+  '''TODO: construct a list of output sequences for the training batch'''
+  output_batch = [vectorized_songs[i+1:i+1+seq_length] for i in idx]
+
+  # x_batch, y_batch provide the true inputs and targets for network training
+  x_batch = np.reshape(input_batch, [batch_size, seq_length])
+  y_batch = np.reshape(output_batch, [batch_size, seq_length])
+  return x_batch, y_batch
+
+
+# Perform some simple tests to make sure your batch function is working properly! 
+test_args = (vectorized_songs, 10, 2)
+if not mdl.lab1.test_batch_func_types(get_batch, test_args) or \
+   not mdl.lab1.test_batch_func_shapes(get_batch, test_args) or \
+   not mdl.lab1.test_batch_func_next_step(get_batch, test_args): 
+   print("======\n[FAIL] could not pass tests")
+else: 
+   print("======\n[PASS] passed all tests!")
+
+
+x_batch, y_batch = get_batch(vectorized_songs, seq_length=5, batch_size=1)
+
+for i, (input_idx, target_idx) in enumerate(zip(np.squeeze(x_batch), np.squeeze(y_batch))):
+    print("Step {:3d}".format(i))
+    print("  input: {} ({:s})".format(input_idx, repr(idx2char[input_idx])))
+    print("  expected output: {} ({:s})".format(target_idx, repr(idx2char[target_idx])))
