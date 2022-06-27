381ee786f7951079cf9a9a6b657cd2b12a1489e4
fadel <fadelsougou@gmail.com>
2022-06-28 00:12:36 +0200

    RNN training

--- src/mel_RNN.py
+++ src/mel_RNN.py
@@ -19,8 +19,8 @@ songs = mdl.lab1.load_training_data()
 
 # Print one of the songs to inspect it in greater detail!
 example_song = songs[0]
-print("\nExample song: ")
-print(example_song)
+# print("\nExample song: ")
+# print(example_song)
 
 
 # Join our list of song strings into a single string containing all songs
@@ -28,16 +28,16 @@ songs_joined = "\n\n".join(songs)
 
 # Find all unique characters in the joined string
 vocab = sorted(set(songs_joined))
-print("There are", len(vocab), "unique characters in the dataset")
+#print("There are", len(vocab), "unique characters in the dataset")
 
 
 char2idx = {u:i for i, u in enumerate(vocab)}
 idx2char = np.array(vocab)
 
-print('{')
-for char,_ in zip(char2idx, range(20)):
-    print('  {:4s}: {:3d},'.format(repr(char), char2idx[char]))
-print('  ...\n}')
+# print('{')
+# for char,_ in zip(char2idx, range(20)):
+#     print('  {:4s}: {:3d},'.format(repr(char), char2idx[char]))
+# print('  ...\n}')
 
 
 def vectorize_string(string):
@@ -46,7 +46,7 @@ def vectorize_string(string):
 
 vectorized_songs = vectorize_string(songs_joined)
 
-print ('{} ---- characters mapped to int ----> {}'.format(repr(songs_joined[:10]), vectorized_songs[:10]))
+#print ('{} ---- characters mapped to int ----> {}'.format(repr(songs_joined[:10]), vectorized_songs[:10]))
 # check that vectorized_songs is a numpy array
 assert isinstance(vectorized_songs, np.ndarray), "returned result should be a numpy array"
 
@@ -72,13 +72,13 @@ def get_batch(vectorized_songs, seq_length, batch_size):
 
 
 # Perform some simple tests to make sure your batch function is working properly! 
-test_args = (vectorized_songs, 10, 2)
-if not mdl.lab1.test_batch_func_types(get_batch, test_args) or \
-   not mdl.lab1.test_batch_func_shapes(get_batch, test_args) or \
-   not mdl.lab1.test_batch_func_next_step(get_batch, test_args): 
-   print("======\n[FAIL] could not pass tests")
-else: 
-   print("======\n[PASS] passed all tests!")
+# test_args = (vectorized_songs, 10, 2)
+# if not mdl.lab1.test_batch_func_types(get_batch, test_args) or \
+#    not mdl.lab1.test_batch_func_shapes(get_batch, test_args) or \
+#    not mdl.lab1.test_batch_func_next_step(get_batch, test_args): 
+#    print("======\n[FAIL] could not pass tests")
+# else: 
+#    print("======\n[PASS] passed all tests!")
 
 
 x_batch, y_batch = get_batch(vectorized_songs, seq_length=5, batch_size=1)
@@ -87,3 +87,155 @@ for i, (input_idx, target_idx) in enumerate(zip(np.squeeze(x_batch), np.squeeze(
     print("Step {:3d}".format(i))
     print("  input: {} ({:s})".format(input_idx, repr(idx2char[input_idx])))
     print("  expected output: {} ({:s})".format(target_idx, repr(idx2char[target_idx])))
+
+
+
+def LSTM(rnn_units): 
+    return tf.keras.layers.LSTM(
+        rnn_units, 
+        return_sequences=True, 
+        recurrent_initializer='glorot_uniform',
+        recurrent_activation='sigmoid',
+        stateful=True,
+    )
+
+from tensorflow.keras.layers import Dense
+
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+    model = tf.keras.Sequential([
+        # Layer 1: Embedding layer to transform indices into dense vectors 
+        #   of a fixed embedding size
+        tf.keras.layers.Embedding(vocab_size, embedding_dim, batch_input_shape=[batch_size, None]),
+
+        # Layer 2: LSTM with `rnn_units` number of units. 
+        # TODO: Call the LSTM function defined above to add this layer.
+        LSTM(rnn_units),
+
+        # Layer 3: Dense (fully-connected) layer that transforms the LSTM output
+        #   into the vocabulary size. 
+        # TODO: Add the Dense layer.
+        Dense(vocab_size)
+    ])
+
+    return model
+
+# Build a simple model with default hyperparameters. You will get the 
+#   chance to change these later.
+model = build_model(len(vocab), embedding_dim=256, rnn_units=1024, batch_size=32)
+
+model.summary()
+
+
+x, y = get_batch(vectorized_songs, seq_length=100, batch_size=32)
+pred = model(x)
+# print("Input shape:      ", x.shape, " # (batch_size, sequence_length)")
+# print("Prediction shape: ", pred.shape, "# (batch_size, sequence_length, vocab_size)")
+
+
+sampled_indices = tf.random.categorical(pred[0], num_samples=1)
+sampled_indices = tf.squeeze(sampled_indices,axis=-1).numpy()
+
+
+# print("Input: \n", repr("".join(idx2char[x[0]])))
+# print()
+# print("Next Char Predictions: \n", repr("".join(idx2char[sampled_indices])))
+
+
+def compute_loss(labels, logits):
+  loss = tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True) 
+  return loss
+
+example_batch_loss = compute_loss(x, pred) 
+
+# print("Prediction shape: ", pred.shape, " # (batch_size, sequence_length, vocab_size)") 
+# print("scalar_loss:      ", example_batch_loss.numpy().mean())
+
+
+# Optimization parameters:
+num_training_iterations = 1000  # Increase this to train longer
+batch_size = 4  # Experiment between 1 and 64
+seq_length = 100  # Experiment between 50 and 500
+learning_rate = 5e-3  # Experiment between 1e-5 and 1e-1
+
+# Model parameters: 
+vocab_size = len(vocab)
+embedding_dim = 256 
+rnn_units = 1024  # Experiment between 1 and 2048
+
+# Checkpoint location: 
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "my_ckpt")
+
+
+### Define optimizer and training operation ###
+
+'''TODO: instantiate a new model for training using the `build_model`
+  function and the hyperparameters created above.'''
+model = build_model(vocab_size, embedding_dim, rnn_units, batch_size)
+
+'''TODO: instantiate an optimizer with its learning rate.
+  Checkout the tensorflow website for a list of supported optimizers.
+  https://www.tensorflow.org/api_docs/python/tf/keras/optimizers/
+  Try using the Adam optimizer to start.'''
+optimizer = tf.keras.optimizers.Adam(learning_rate)
+
+@tf.function
+def train_step(x, y): 
+  # Use tf.GradientTape()
+  with tf.GradientTape() as tape:
+  
+    '''TODO: feed the current input into the model and generate predictions'''
+    y_hat = model(x)
+  
+    '''TODO: compute the loss!'''
+    loss = compute_loss(y, y_hat)
+
+  # Now, compute the gradients 
+  '''TODO: complete the function call for gradient computation. 
+      Remember that we want the gradient of the loss with respect all 
+      of the model parameters. 
+      HINT: use `model.trainable_variables` to get a list of all model
+      parameters.'''
+  grads = tape.gradient(loss, model.trainable_variables)
+  
+  # Apply the gradients to the optimizer so it can update the model accordingly
+  optimizer.apply_gradients(zip(grads, model.trainable_variables))
+  return loss
+
+##################
+# Begin training!#
+##################
+
+history = []
+plotter = mdl.util.PeriodicPlotter(sec=2, xlabel='Iterations', ylabel='Loss')
+if hasattr(tqdm, '_instances'): tqdm._instances.clear() # clear if it exists
+
+import matplotlib.pyplot as plt
+fig, axs = plt.subplots()
+axs.legend(('Predicted', 'True'))
+axs.set_xlabel('Iteration')
+axs.set_ylabel('x value')
+
+for iter in tqdm(range(num_training_iterations)):
+
+  # Grab a batch and propagate it through the network
+  x_batch, y_batch = get_batch(vectorized_songs, seq_length, batch_size)
+  loss = train_step(x_batch, y_batch)
+
+  # Update the progress bar
+  history.append(loss.numpy().mean())
+  axs.plot(history)
+  plt.savefig("./training_checkpoints/learning_rate.png")
+
+  # Update the model with the changed weights!
+  if iter % 100 == 0:     
+    model.save_weights(checkpoint_prefix)
+
+    
+    
+# Save the trained model and the weights
+model.save_weights(checkpoint_prefix)
+
+
+
+
